30ca4d7b216673facd449ad9a1ee100a100b2835
h.izzouzi@lemon-interactive.fr <l3m0n1nt3r@ct1v3>
2019-02-14 12:20:46 +0100

    added doctrine structure and a few property files

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Property;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,7 +15,27 @@ class PropertyController extends AbstractController {
      * @return Response
      */
     public function index(): Response {
-        return $this->render( 'property/index.html.twig' );
+        // $property = new Property();
+        // $property->setTitle('Mon premier bien')
+        //     ->setPrice(200000)
+        //     ->setRooms(4)
+        //     ->setBedrooms(3)
+        //     ->setDescription('Une petite descriptions.')
+        //     ->setSurface(60)
+        //     ->setFloor(4)
+        //     ->setHeat(1)
+        //     ->setCity('Montpellier')
+        //     ->setAddress('8 rue du ballon')
+        //     ->setPostalCode(34000);
+
+        // $em = $this->getDoctrine()->getManager();
+        // $em->persist($property);
+        // $em->flush();
+
+        $repository = $this->getDoctrine()->getRepository(Property::class);
+        dump($repository);
+
+        return $this->render( 'property/index.html.twig', [ 'current-menu' => 'properties' ] );
     }
 
 }
