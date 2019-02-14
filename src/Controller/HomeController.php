b4a0f3eb95bd6e7ffdff383296dabf029df0f454
h.izzouzi@lemon-interactive.fr <l3m0n1nt3r@ct1v3>
2019-02-14 10:39:33 +0100

    home page and page acheter

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,25 +2,18 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use Twig_Environment;
 
-class HomeController {
-
-    /**
-     * @var Twig_Environment
-     */
-    private $twig;
-
-    public function __construct( Twig_Environment $twig ) {
-        $this->twig = $twig;
-    }
+class HomeController extends AbstractController {
 
     /**
      * @Route("/", name="home")
      * @return Response
      */
     public function index(): Response {
-        return new Response( $this->twig->render( 'pages/home.html.twig' ) );
+        return $this->render( 'pages/home.html.twig' );
     }
 }
